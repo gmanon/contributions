152662a2d2d7a6e1950c5e627a9cd00d884e6589
GManon <ggonjon@gmail.com>
2016-01-23 17:19:03 -0500

    Create template.inc.php
    Main template page for contributions

---
+++ templates/template.inc.php
@@ -0,0 +1,27 @@
+<!-- Basic template layout -->
+<!-- Author: Guillermina Gonjon
+     Date: 01/23/2016
+     Page: Main_Template
+     App Name: Contributions
+-->
+
+<html DOCTYPE!>
+<html>
+  <head>
+    <title><!--{ PAGE_TITLE }--></title>
+    <!--{ JAVASCRIPT }-->
+    <!--{ STYLE }-->
+  </head>
+  <body>
+    <header>
+      <div class="menu"><!--{ TOP_MENU }--></div><div class="menu"><!--{ USER_MENU }--></div>
+      <div class="search"><!--{ PAGE_SEARCH }--></div>
+      <div class="main-menu"><!--{ MAIN_MENU }--></div>
+    </header>
+      <div class="page"><!--{ CONTENT_PAGE }--></div>
+      <div class="forms"><!--{ FORMS }--></div>
+    <footer>
+      <div class="footer"><!--{ FOOTER }--></div>
+    </footer>
+  </body>
+</html>

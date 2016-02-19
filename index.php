1e03a8ba9044ad13a463dd7ba89b1387df25a4be
GManon <ggonjon@gmail.com>
2016-02-19 15:27:54 -0500

    Added files via upload

--- index.php
+++ index.php
@@ -8,15 +8,33 @@
  ** @license: GPL
 **/
 
-require_once '../initializer.inc.php';
+require_once './initializer.inc.php';
 $main_files = array(
-                    CONFIG_DIRECTORY=>'configDB.class.php',
-                    STYLE_DIRECTORY=>'style.css',
-                    TEMPLATES_DIRECTORY=>'GManonTemplate.class.php',
-                    TEMPLATES_DIRECTORY=>'template.inc.php'
+                    CONFIG_DIRECTORY=>'configDb.class.php',
+                    //STYLE_DIRECTORY=>'style.css',
+                    
                     );
+require_once('./templates/GManonTemplate.class.php');
+
+$style = file_get_contents('./templates/style/style.css');
+
+$vars = array(
+		'PAGE_TITLE'=>'This is my title',
+		'STYLE'=>$style,
+		'TOP_MENU'=>'HOME | CONTACT | WE | WHATYOULIKE',
+		'USER_MENU'=>'ME | YO | WHATIAMALLOWED',
+		'PAGE_SEARCH'=>'Search Form goes here',
+		'MAIN_MENU'=>'Catalog | SeeThis | Continue here',
+		'CONTENT_PAGE'=>'The lines come here.  This is the actual content.',
+		'FOOTER'=>'Get this at the foot of the page'
+);
+
+$gmtemplate = new GManonTemplate();
+
+echo $gmtemplate->replaceVars($vars, "./templates/template.inc.php");
+
 foreach($main_files as $directory=>$file)
 {
-    check_file($directory=>$file);
+    check_file($directory,$file);
 }
 ?>
